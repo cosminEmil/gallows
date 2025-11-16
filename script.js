026bbede804f919e56ca530d3175577473fb5aeb
Cosmin Emil Bucur <cosminbucur2312@gmail.com>
2025-11-16 10:35:11 +0200

    The word is randomly selected and the communicationis done via html element

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-let letters = [];
+let letters = []; let words = ["car", "animal", "house", "iphone", "macbook", "coffee"];
+let winnerCnt = 0;
 
 function generateGame() {
-    let word = document.getElementById('word').value;
+    let randomNr = Math.floor(Math.random() * words.length);
+    let word = words[randomNr];
     for (let i = 0; i < word.length; ++i) {
         letters.push(word[i]);
         let letter = document.createElement("input");
@@ -14,20 +16,25 @@ function generateGame() {
     }
 }
 
+
 function checkLetter() {
     let unCheckedLetter = document.getElementById('unCheckedLetter').value;
+    let message = document.getElementById("message");
     if (letters.includes(unCheckedLetter)) {
         let index = letters.indexOf(unCheckedLetter);
         let letter = document.getElementById(index);
-        console.log(letter);
         letter.value = unCheckedLetter;
         letters[index] = "";
+        ++winnerCnt;
+        if (winnerCnt == letters.length) {
+            message.innerText = "you're lucky mate, you won this time."
+        }
     } else {
         let lifesNr = document.getElementById('lifesNr').value; 
         if (lifesNr > 1) {
             document.getElementById('lifesNr').stepDown(1);
         } else {
-            alert("ai murit");
+            message.innerText = "you're dead mate";
         }
     }
 }
